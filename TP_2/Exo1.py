154b39c53637e6f32866e9d442f1670d7c6b41ed
FelixMercier <felixmercier31@gmail.com>
2024-11-25 23:24:39 +0100

    simplex fonctionel. Fin TP2

--- TP_2/Exo1.py
+++ TP_2/Exo1.py
@@ -10,30 +10,34 @@ def simplex(A, nb_para, B):
     """
     A : matrice A du problème B = AX + V
     X : idem
-    B : idem, les b_i doivent être positifs ou nuls
+    B : idem
     """
+    n=len(A)
+    p=nb_para
     
-    # V = B - A @ X
-
-    # U = [V[i] if V[i] >= 0 else 0 for i in range(0, len(V))]
-    # W = [-V[i] if V[i]<=0 else 0 for i in range(0, len(V))]
-    
-    # X1 = [X[i] if X[i] >= 0 else 0 for i in range(0, len(X))]
-    # X2 = [-X[i] if X[i]<=0 else 0 for i in range(0, len(X))]
+    A_eq = np.sign(B) * A
+    A_eq = np.hstack((A_eq, -A_eq, np.eye(len(A)), -np.eye(len(A))))
     
     c = np.hstack(( np.zeros((2*nb_para,)) , np.ones((2*len(B),)) ))
+
+    B_eq = np.abs(B)
     
-    A_eq = np.hstack((A, -A, np.eye(len(A)), np.eye(len(A))))
-    B_eq = B
-    
-    sol = linprog(c, A_eq=A_eq, b_eq=B_eq, method='simplex')
+    sol = linprog(c, A_eq=A_eq, b_eq=B_eq, method='highs')
     
     n_iter = sol.nit
     para = sol.x
     Xchap = para[0:nb_para] - para[nb_para : 2* nb_para]
-    Vchap = B - A@Xchap
     
-    return n_iter, Xchap, Vchap
+    Vchap = B - A@Xchap.reshape((nb_para, 1))   
+    
+    sigma02 = Vchap.T@P@Vchap / (n-p)
+    
+    VarXchap = sigma02*np.linalg.inv(A.T@P@A)
+    VarVchap = sigma02*(np.linalg.inv(P) - A@np.linalg.inv(A.T@P@A)@A.T)
+    
+    Vnor = np.linalg.inv(np.sqrt(np.diag(np.diag(VarVchap)))) @ Vchap
+    
+    return n_iter, Xchap, Vchap, Vnor, VarXchap
 
 def ransac(A, B, P, n, t, T, K):
     nb_bonnes_mes_abs = [1]
@@ -65,19 +69,6 @@ def ransac(A, B, P, n, t, T, K):
 xmes, ybruit = tp.creer_mesures(2, 5, -50, 50, 100, 1, 0, 0, 0)
 vecx = xmes.reshape(len(xmes), 1)
 vecy = ybruit.reshape(len(ybruit), 1)
-
-A = np.hstack(( vecx, np.ones(np.shape(vecx)) ))
-
-nb_para = 2
-
-B = np.abs(vecy)
-
-
-sigma = 5
-P = sigma*np.eye(len(vecx))
-
-
-
         
 if __name__ == '__main__' : 
     
@@ -85,6 +76,13 @@ if __name__ == '__main__' :
     
     if estimation_method == 'ransac':
         
+        A = np.hstack(( vecx, np.ones(np.shape(vecx)) ))
+
+        B = np.abs(vecy)
+
+        sigma = 5
+        P = sigma*np.eye(len(vecx))
+
         print('Estimation par ransac')
         
         Xchap, Vchap, n_iter = ransac(A, vecy, P, 2, 1.96, 95, 100)
@@ -105,10 +103,15 @@ if __name__ == '__main__' :
         plt.title("Résidus de l'algo Ransac")
         
     if estimation_method == 'simplex':
-        
         print("Estimation par simplex modifié")
+
+        A = np.hstack((vecx, np.ones(vecx.shape)))
         
-        n_iter, Xchap, Vchap = simplex(A, nb_para, B)
+        B = vecy
+        
+        nb_para = 2
+        
+        n_iter, Xchap, Vchap, Vnor, VarXchap = simplex(A, nb_para, B)
 
         est =  plt.figure()
         
@@ -117,9 +120,16 @@ if __name__ == '__main__' :
         
         plt.title('Estimation para simplex modifié, {} itérations'.format(n_iter))
         
-        # res = plt.figure()
-        # plt.scatter(vecx, Vchap)
-        # plt.title('Résidus')
+        res = plt.figure()
+        plt.scatter(vecx, Vchap)
+        plt.title("Résidus de l'estimation par simplex modifié")
+        
+        resnor = plt.figure()
+        plt.hist(Vnor, bins=15, density=True, color='skyblue', edgecolor='black')
+        lin = np.linspace(-4, 4, 2000)
+        mean, std = norm.fit(Vnor)
+        plt.plot(lin, norm.pdf(lin, mean, std), c='black')
+        plt.title("histogramme des résidus normalisés de l'estimation par simplex modifié")
         
         print("a = {}".format(np.round(Xchap[0], 4)))
         print("b = {}".format(np.round(Xchap[1], 4)))
